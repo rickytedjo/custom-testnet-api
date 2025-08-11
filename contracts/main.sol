473838b3b73a29a921d73eadf4a6384cac02d742
rickytedjo <rickyputra.tedjo@gmail.com>
2025-08-11 15:14:16 +0700

    chore:added openzeppelin for contract token. token and all functions related will be added to main contract.

--- contracts/main.sol
+++ contracts/main.sol
@@ -1,32 +1,43 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.19;
+pragma solidity ^0.8.27;
 
-contract P2PTransferProject {
-    address public owner;
-    string public name;
-    modifier onlyOwner() {
-        require(msg.sender == owner, "Only owner");
-        _;
-    }
+import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import {Strings} from "@openzeppelin/contracts/utils/Strings.sol";
 
-    constructor(string memory _name) {
-        owner = msg.sender;
-        name = _name;
-    }
+// contract P2PTransferProject {
+//     address public owner;
+//     string public name;
+//     modifier onlyOwner() {
+//         require(msg.sender == owner, "Only owner");
+//         _;
+//     }
 
-    function getBalance() external view returns (uint256) {
-        return address(this).balance;
-    }
+//     constructor(string memory _name) {
+//         owner = msg.sender;
+//         name = _name;
+//     }
 
-    function setName(string memory _name) external onlyOwner {
-        name = _name;
-    }
+//     function getBalance() external view returns (uint256) {
+//         return address(this).balance;
+//     }
 
-    function getName() external view returns (string memory) {
-        return name;
-    }
+//     function setName(string memory _name) external onlyOwner {
+//         name = _name;
+//     }
 
-    function getOwner() external view returns (address) {
-        return owner;
-    }
+//     function getName() external view returns (string memory) {
+//         return name;
+//     }
+
+//     function getOwner() external view returns (address) {
+//         return owner;
+//     }
+// }
+
+contract ProjectToken is ERC20 {
+    using Strings for uint256;
+
+    constructor() ERC20("ProjectToken", "PTKN") {}
+
+    
 }
